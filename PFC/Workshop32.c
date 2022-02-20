c80c00d3ba7abe11b32f02bb5e412ccc3e577371
Minh Luu <minh.luu@tamu.edu>
2022-02-20 16:53:55 -0600

    Added comment for 32.

--- PFC/Workshop32.c
+++ PFC/Workshop32.c
@@ -2,23 +2,25 @@
 
 int main()
 {
-    int lower, upper;
+    int lower, upper; // Get the lower and upper bounds
     printf("Enter the lower bound: ");
     scanf("%d", &lower);
 
     printf("Enter the upper bound: ");
     scanf("%d", &upper);
+
+    // Iterate through the numbers from lower to upper and check for palindromes
     for (int i = lower; i <= upper; ++i)
     {
-        int rem = 0, reversed = 0, place = 1;
+        int rem = 0, reversed = 0, place = 1; // Use similar concept as part 3 in Workshop31.c
         int temp = i;
-        while (temp > 0)
+        while (temp > 0) // 
         {
             reversed = reversed * 10;
             reversed = reversed + temp % 10;
             temp = temp / 10;
         }
-        if (i == reversed)
+        if (i == reversed) // If the original number is equal to the reversed number, it's a palindrome
         {
             printf("%d is a palindrome\n", i);
         }
