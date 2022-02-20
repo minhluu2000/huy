aa7708585450e44870e7ebb7b3bbaa754b0d1208
Minh Luu <minh.luu@tamu.edu>
2022-02-20 16:50:55 -0600

    Added comment for 42.

--- PFC/Workshop42.c
+++ PFC/Workshop42.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int lower, upper, n_column, counter = 0;
+    int lower, upper, n_column, column_counter = 0; // Get the lower and upper bounds
     printf("Enter the lower bound: ");
     scanf("%d", &lower);
 
@@ -12,38 +12,41 @@ int main()
     printf("Enter the number of columns: ");
     scanf("%d", &n_column);
 
-    if (lower < 2)
+    if (lower < 2) // If lower is less than 2, lower = 2 since that's definition of prime
     {
         lower = 2;
     }
 
+    // Print information
     printf("Table of Primes:\n===============");
     printf("Lower limit: %d\n", lower);
     printf("Upper limit: %d\n", upper);
     printf("# of columns: %d\n", n_column);
 
+    // Print the table
     for (int i = lower; i <= upper; ++i)
     {
-        int temp = i;
-        int is_prime = 1;
-        for (int k = 2; k < temp; ++k)
+        int temp = i;                  // Store the value of i in temp for processing without changing i
+        int is_prime = 1;              // Assume that the number is prime
+        for (int k = 2; k < temp; ++k) // Check if the number is prime
         {
-            if (temp % k == 0)
+            if (temp % k == 0) // If the number is divisible by any number other than 1 and itself, it's not prime
             {
                 is_prime = 0;
                 break;
             }
         }
-        if (is_prime)
+
+        if (is_prime) // If the number is prime, print it
         {
-            printf("%10d", temp);
-            counter++;
+            printf("%10d", temp); // Print the number with 10 spaces
+            ++column_counter;
         }
 
-        if (counter == n_column)
+        if (column_counter == n_column) // If the number of columns is reached, print a new line
         {
             printf("\n");
-            counter = 0;
+            column_counter = 0;
         }
     }
 }
